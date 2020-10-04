0b13bb7ddbc6402d72ac3846933ca4354ec17383
soumyadeepsp <soumyadeepsp@gmail.com>
2020-10-05 02:25:27 +0530

    completed project

---
+++ src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_X1xrntdmgrcjNWP9ebZTPt-cDFykse0",
+    authDomain: "covid-tracker-bc947.firebaseapp.com",
+    databaseURL: "https://covid-tracker-bc947.firebaseio.com",
+    projectId: "covid-tracker-bc947",
+    storageBucket: "covid-tracker-bc947.appspot.com",
+    messagingSenderId: "152695205542",
+    appId: "1:152695205542:web:ca86d9b3081606a23fce67"
+};
